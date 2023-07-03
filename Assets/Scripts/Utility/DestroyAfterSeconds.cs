10b6c36264411c7b90c64726e91ee9dbe52553ae
Semicolon-GD <maerken@icloud.com>
2023-07-03 21:29:10 +0300

    Bullet and Player Control

---
+++ Assets/Scripts/Utility/DestroyAfterSeconds.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour
+{
+
+    public float seconds;
+   
+    void Start()
+    {
+        Destroy(gameObject, seconds);
+    }
+
+   
+}
